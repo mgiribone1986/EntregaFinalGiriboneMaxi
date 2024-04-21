bb61e44bcc440f6c79c12e55900aca610213a463
Maxi Giribone <giribonemaximiliano@gmail.com>
2024-04-21 18:35:01 -0300

    Subida Doming

--- src/app/layouts/auth/auth.module.ts
+++ src/app/layouts/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { SharedModule } from '../../shared/shared.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, SharedModule, AuthRoutingModule],
+  imports: [CommonModule, SharedModule, AuthRoutingModule,MatCardModule,MatIconModule ],
 })
 export class AuthModule {}
